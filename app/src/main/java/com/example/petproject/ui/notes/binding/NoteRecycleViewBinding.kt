7582ccc3cff27361134b5a34e3d2df4dc493292b
Vladyslav Lukiianchuk <vladlukiyanchukoo@gmail.com>
2021-05-25 19:15:53 +0300

    glide is connected in notes list

--- app/src/main/java/com/example/petproject/ui/notes/binding/NoteRecycleViewBinding.kt
+++ app/src/main/java/com/example/petproject/ui/notes/binding/NoteRecycleViewBinding.kt
@@ -1,9 +1,12 @@
 package com.example.petproject.ui.notes.binding
 
+import android.content.res.Resources
+import android.util.Log
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.example.petproject.ui.notes.adapters.RecycleViewItemDecoration
 
 object NoteRecycleViewBinding {
@@ -22,10 +25,18 @@ object NoteRecycleViewBinding {
         this.addItemDecoration(decoration)
     }
 
-    @BindingAdapter(value = ["app:newUrl"])
+    @BindingAdapter(value = ["newUrl"])
     @JvmStatic
-    fun setNewUrl(view: ImageView) {
-        Glide.with(view).load("").into(view)
+    fun ImageView.setNewUrl(some: String) {
+        val window = Resources.getSystem().getDisplayMetrics()
+        val url = "https://picsum.photos/${window.widthPixels}/${window.heightPixels}/?random"
+        //val url = "https://i.picsum.photos/id/642/1080/2131.jpg?hmac=DQ7IJEeznMxwqHnip22KYVto2dQ1SrFszmi1oJT69C8"
+        Log.d("tag", url)
+        Glide.with(this)
+            .load(url)
+            .skipMemoryCache(true)
+            .centerCrop()
+            .into(this)
     }
 
 }
